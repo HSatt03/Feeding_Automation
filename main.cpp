6407099c6a54bf4a76d8c01e5042354b87c8c1c1
Hasti <hsattari2006@gmail.com>
2025-04-25 12:58:34 +0330

    All setters

--- main.cpp
+++ main.cpp
@@ -567,3 +567,45 @@ private:
     string _address;
     int _capacity;
 };
+
+DiningHall::DiningHall(int hall_id = 2, string name = "Sadaf", string address = "Next to the Central Library", int capacity = 100)
+{
+    _hall_id = hall_id;
+    _name = name;
+    _address = address;
+    _capacity = capacity;
+}
+void DiningHall::setHallId(int hall_id)
+{
+    if(hall_id == 1 && hall_id == 2)
+    {
+        _hall_id = hall_id;
+    }
+    else 
+    {
+        throw invalid_argument("Incorect value for hall_id!!!");
+    }
+}
+void DiningHall::setName(string name)
+{
+    int value = name.length();
+    for(int i = 0; i < value; i++)
+    {
+        if(!isalpha(name[i]))
+        {
+            throw invalid_argument("Incorrect name!!!");
+        }
+    }
+    _name = name;
+}
+void DiningHall::setAddress(string address)
+{
+    _address = address;
+}
+void DiningHall::setCapacity(int capacity)
+{
+    if(capacity > 0)
+    {
+        _capacity = capacity;
+    }
+}
