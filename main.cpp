8d4972545642489cfae073fa40b84ea8155d37f6
Hasti <hsattari2006@gmail.com>
2025-04-25 11:42:11 +0330

    End of declaration of the functions and variables of the class

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+class Student
+{
+public:
+    Student(int user_id = 12, string student_id = "4022559082", string name = "name", string email = "name20002001@gmail.com", float balance = 100000, bool is_active = false);
+
+    void setUserId(int);
+    void setStudentId(string);
+    void setName(string);
+    void setEmail(string);
+    void setBalance(float);
+    void setIsActive(bool);
+    void setReservations(vector<Reservation>);
+
+    void print()const;
+    void reserveMeal(Meal);
+    bool cancelReservation(Reservation);
+    bool operator==(Student);
+    int getUserId()const
+    {
+        return _user_id;
+    }
+    string getStudentId()const
+    {
+        return _student_id;
+    }
+    string getName()const
+    {
+        return _name;
+    }
+    string getEmail()const
+    {
+        return _email;
+    }
+    float getBalance()const
+    {
+        return _balance;
+    }
+    bool getIsActive()const
+    {
+        return _is_active;
+    }
+    const vector<string>& getReservations()const
+    {
+        return _reservations;
+    }
+
+private:
+    int _user_id;
+    string _student_id;
+    string _name;
+    string _email;
+    float _balance;
+    bool _is_active;
+    vector<Reservation*>_reservations;
+};
