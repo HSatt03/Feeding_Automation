a94ebda0d2e63721cebb7c494cb87eac7819b855
Hasti <hsattari2006@gmail.com>
2025-04-26 13:03:43 +0330

    diningHall.cpp

---
+++ diningHall.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "diningHall.hpp"
+using namespace std;
+DiningHall::DiningHall(int hall_id = 2, string name = "Sadaf", string address = "Next to the Central Library", int capacity = 100)
+{
+    _hall_id = hall_id;
+    _name = name;
+    _address = address;
+    _capacity = capacity;
+}
+void DiningHall::setHallId(int hall_id)
+{
+    if(hall_id == 1 && hall_id == 2)
+    {
+        _hall_id = hall_id;
+    }
+    else 
+    {
+        throw invalid_argument("Incorect value for hall_id!!!");
+    }
+}
+void DiningHall::setName(string name)
+{
+    int value = name.length();
+    for(int i = 0; i < value; i++)
+    {
+        if(!isalpha(name[i]))
+        {
+            throw invalid_argument("Incorrect name!!!");
+        }
+    }
+    _name = name;
+}
+void DiningHall::setAddress(string address)
+{
+    _address = address;
+}
+void DiningHall::setCapacity(int capacity)
+{
+    if(capacity > 0)
+    {
+        _capacity = capacity;
+    }
+}
+void DiningHall::print()const
+{
+    cout << "Name: " << _name << 
+                            "\nHall Id: " << _hall_id << 
+                                                    "\nAddress: " << _address << 
+                                                                            "\nCapacity: " << _capacity << endl;            
+}
