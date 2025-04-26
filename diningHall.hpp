7ccb8b1b67fa424dd510ac723c20e9134527b9ea
Hasti <hsattari2006@gmail.com>
2025-04-26 12:37:12 +0330

    headerfile_diningHall/*student

---
+++ diningHall.hpp
@@ -0,0 +1,39 @@
+#ifndef DININGHALL_H
+#define DININGHALL_H
+#include <iostream>
+#include <string>
+using namespace std;
+class DiningHall
+{
+public:
+    DiningHall(int, string, string, int);
+    void setHallId(int);
+    void setName(string);
+    void setAddress(string);
+    void setCapacity(int);
+    void print()const;
+    bool operator==(DiningHall);
+    int getHallId()
+    {
+        return _hall_id;
+    }
+    string getName()
+    {
+        return _name;
+    }
+    string getAddress()
+    {
+        return _address;
+    }
+    int getCapacity()
+    {
+        return _capacity;
+    }
+
+private:
+    int _hall_id;
+    string _name;
+    string _address;
+    int _capacity;
+};
+#endif
