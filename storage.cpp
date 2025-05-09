e92a68000b56e79e8cf39cfbeb5a223cf7a10de8
Fatemeh1522 <fanoos185472@gmail.com>
2025-05-09 16:46:28 +0330

    cpp of storage

---
+++ storage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "storage.hpp"
+using namespace std; 
+Storage::Storage(int mealIDCounter, int diningHallIDCounter)
+{
+    setMealIDCounter(mealIDCounter);
+    setDiningHallIDCounter(diningHallIDCounter);
+}
+void Storage::setMealIDCounter(int m)
+{
+    _mealIDCounter = (m > 0) ? m : 0;
+}
+void Storage::setDiningHallIDCounter(int d)
+{
+    _diningHallIDCounter = (d > 0) ? d : 0;
+}
+static Storage&::Storage instance()
+{
+    static Storage instance;
+    return instance;
+}
