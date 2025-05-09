bb4101e4a8385520c21f380ddc1f879d661cff33
Fatemeh1522 <fanoos185472@gmail.com>
2025-05-09 16:44:54 +0330

    headerfile storage

---
+++ storage.hpp
@@ -0,0 +1,33 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#include <vector>
+using namespace std;
+class Meal;
+class DiningHall;
+
+class Storage
+{
+public:
+    void setMealIDCounter(int);
+    void setDiningHallIDCounter(int);
+
+    vector<Meal> allMeals;
+    vector<DiningHall> allDiningHall;
+    static Storage& instance();
+    int getMealIDCounter()const
+    {
+        return _mealIDCounter;
+    }
+    int getDiningHallIDCounter()const
+    {
+        return _diningHallIDCounter;
+    }
+
+private:
+    int _mealIDCounter;
+    int _diningHallIDCounter;
+    Storage(int = 0, int = 0);
+    Storage(const Storage&) = delete;
+    Storage operator=(const Storage&) = delete;
+};
+#endif
