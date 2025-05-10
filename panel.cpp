60de209e6153bb0bd459f451d000d6dd09838c4d
Fatemeh1522 <fanoos185472@gmail.com>
2025-05-10 16:36:47 +0330

    panel.cpp

---
+++ panel.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "panel.hpp"
+using namespace std;
+bool panel::Action(bool b)
+{
+
+}
+void panel::showMenu()
+{
+
+}
+void panel::showStudentInfo()
+{
+
+}
+void panel::checkBalance()
+{
+
+}
+void panel::viewReservation()
+{
+
+}
+void panel::addviewReservation(Reservation)
+{
+
+}
+void panel::addToShoppingCart()
+{
+
+}
+void panel::confirmShoppingCart()
+{
+
+}
+void panel::removeShoppingCartItem()
+{
+
+}
+void panel::increaseBalance()
+{
+
+}
+void panel::viewRecentTransactions()
+{
+
+}
+void panel::cancelReservation(int)
+[
+
+]
+void panel::exit(bool b)
+{
+
+}
