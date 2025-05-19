4a1126bad37a309f6439ecf18e4c82da4f9badce
Hasti <hsattari2006@gmail.com>
2025-05-19 17:26:44 +0330

    finish sessionManager.cpp/Update other classes

--- include/sessionManager.hpp
+++ include/sessionManager.hpp
@@ -6,36 +6,35 @@
 #include "../include/shoppingCart.hpp"
 using namespace std;
 
+namespace StudentSession
+{
 class SessionManager : public SessionBase
 {
 public:
-    static SessionManager instance();
+    static SessionManager& instance();
 
-    Student* getCurrentStudsent()const
-    {
-        return _currentStudent;
-    }
-    ShoppingCart* getShoppingCart()const
-    {
-        return _shopping_cart;
-    }
     int getStudentID()const
     {
         return _studentID;
     }
 
+    void login(string, string)override;
     void logout()override;
-    Student currentStudent();
-    ShoppingCart shoppingCart();
+    Student* currentStudent();
+    ShoppingCart* shoppingCart();
 
 private:
-    Student *_currentStudent;
-    ShoppingCart *_shopping_cart;
+    Student* _currentStudent;
+    ShoppingCart* _shopping_cart;
     int _studentID;
+
+    SessionManager();
+    SessionManager(const SessionManager&) = delete;
+    SessionManager& operator=(const SessionManager&) = delete;
     void load_session()override;
     void save_session()override;
    
-    
 };
+}
 
 #endif
--- src/sessionBase.cpp
+++ src/sessionBase.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "../include/sessionBase.hpp"
+using namespace std;
+
 void SessionBase::setCreatedAt(time_t createdAt)
 {
     if(createdAt > 0)
