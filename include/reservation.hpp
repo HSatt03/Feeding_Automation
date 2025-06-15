ad5222fb1b6f7c71fd69192007d516f6201b2d87
Hasti <hsattari2006@gmail.com>
2025-06-15 11:10:01 +0330

    Update previous classes

--- include/reservation.hpp
+++ include/reservation.hpp
@@ -19,13 +19,14 @@ enum class RStatus
 class Reservation
 {
 public:
-    Reservation(DiningHall*, Meal*, int = 0, RStatus = RStatus::FAILED, time_t = time(0)); 
+    Reservation(DiningHall*, Meal*, int = 0, RStatus = RStatus::FAILED, time_t = time(0), time_t = time(0)); 
 
     void setReservation_id(int);
     void setDhall(DiningHall*);
     void setMeal(Meal*);
     void setStatus(RStatus);
-    void setTime(time_t);
+    void setCreatedTime(time_t);
+    void setRemovedTime(time_t);
 
     int getReservation_id()const
     {
@@ -43,11 +44,14 @@ public:
     {
         return _status;
     }
-    time_t getTime()const
+    time_t getCreatedTime()const
     {
         return _created_at;
     }
-
+    time_t getRemovedTime()const
+    {
+        return _removed_from;
+    }
     void print()const;
     bool cancel();
     bool operator==(Reservation);
@@ -58,7 +62,7 @@ private:
     DiningHall *_dHall;
     Meal *_meal;
     RStatus _status;
-    time_t _created_at;
+    time_t _created_at, _removed_from;
 
 };
 
