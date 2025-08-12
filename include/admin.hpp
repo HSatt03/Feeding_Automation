c780cd8f24584b239bbd1a505434d0816991f437
Fatemeh1522 <fanoos185472@gmail.com>
2025-08-12 10:57:24 +0330

    Update Admin class

--- include/admin.hpp
+++ include/admin.hpp
@@ -1,15 +1,20 @@
 #ifndef ADMIN_HPP
 #define ADMIN_HPP
 #include <iostream>
+#include "../include/user.hpp"
 using namespace std;
 
-class Admin
+class Admin : public User
 {
 public:
+    Admin(int = 0, string = "", string = "", string = "", string = "");
     void print()const;
     string getType();
-private:
+    static bool isThereAnyAdmin();
+    static bool sign_in();
+    static void register_admin(string&, string&);
 
+private:
 };
 
 #endif
